fbdd027404c46167e98bf1d15923793d70fdfe43
Vitaliy Semenov <synthex@bk.ru>
2023-04-19 15:44:11 +0600

    fix: truncated description up to 100 chars

--- resources/views/front/layouts/chunks/post-card.blade.php
+++ resources/views/front/layouts/chunks/post-card.blade.php
@@ -5,9 +5,8 @@
         <h5 class="text-uppercase fs-6 fw-bold mb-4">{{ $post->category->title }}</h5>
       </header>
       <h4 class="card-title fw-bold mb-3"><a href="{{ route('blog.page', ['slug' => $post->slug]) }}" class="blog-post" itemprop="headline">{{ $post->title }}</a></h4>
-      <p class="card-description">{!! $post->description !!}</p>
+      <p class="card-description">{!! \Illuminate\Support\Str::limit($post->description, 100, $end='...') !!}</p>
     </div>
-
     <footer class="card-footer">
       <span  itemprop="author" itemscope itemtype="https://schema.org/Person"><a itemprop="url" href="https://example.com/profile/johndoe123"><img src="https://source.unsplash.com/25x25/?businessman" class="rounded-circle img-responsive img-fluid me-2">
       <span class="card-text">Алексей Алексеев</span></a></span>
