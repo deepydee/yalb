ce39ac8cbf5eb0601371cf93d12c51aa74b96333
Vitaliy Semenov <synthex@bk.ru>
2023-05-17 18:21:19 +0600

    feat: add ability to populate categories table from JSON

---
+++ database/seeders/CategorySeederJSON.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Storage;
+
+class CategorySeederJSON extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        $json = Storage::disk('local')->get('public/categories.json');
+        $categories = json_decode($json, true);
+
+        foreach ($categories as $category) {
+           Category::create($category);
+        }
+
+        $categories = Category::all();
+
+        foreach ($categories as $category) {
+            $category->generatePath();
+
+            if (isset($category['thumb'])) {
+                $mediaPath = storage_path("app/public/uploads/images/" . $category['thumb']);
+                    $category->addMedia($mediaPath)
+                        ->preservingOriginal()
+                        ->toMediaCollection('images');
+            }
+
+            $category->update([
+                'description' => fake()->realText(500),
+                'keywords' => fake()->words(fake()->numberBetween(3, 9)),
+                'content' => fake()->realText(1500),
+            ]);
+        }
+    }
+}
